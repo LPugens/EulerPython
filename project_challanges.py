a8e84941a58022770e290fab4b0644979713f787
Lucas Pugens Fernandes <lfernandes@nelogica.com.br>
2021-05-30 19:42:35 -0300

    challenge_18 done

--- project_challanges.py
+++ project_challanges.py
@@ -494,5 +494,49 @@ def challenge_17():
     return length
 
 
+def challenge_18():
+    triangle = '''75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
+
+    totals = []
+    for i_row, row in enumerate(triangle.split('\n')):
+        row = row.split(' ')
+        int_row = [int(cell) for cell in row]
+
+        if i_row == 0:
+            totals.append(int_row)
+            continue
+
+        row_above = totals[-1]
+        totals += [[]]
+        for k_row, cell in enumerate(int_row):
+            if k_row != 0:
+                path_above_left = row_above[k_row-1]
+            else:
+                path_above_left = -1
+            if k_row < len(row_above):
+                path_above_right = row_above[k_row]
+            else:
+                path_above_right = -1
+
+            max_path = max(path_above_left, path_above_right)
+            totals[-1].append(max_path + cell)
+
+    return max(totals[-1])
+
+
 if __name__ == '__main__':
-    print(f'result: {challenge_17()}')
+    print(f'result: {challenge_18()}')
